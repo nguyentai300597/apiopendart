fc0a14ba8e7077460c55119ea98eeb524edbe83e
tai.nt <nguyenductaiqn97@gmail.com>
2022-12-30 16:03:56 +0700

    init

--- update_json.sh
+++ update_json.sh
@@ -1,3 +1,3 @@
 curl -s https://?? -o ./openapi/apitest/open_api/api_sw.json
 
-flutter pub run build_runner build --delete-conflicting-outputs
+flutter pub run build_runner build --delete-conflicting-outputs 
